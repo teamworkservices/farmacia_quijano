603e416f8d16f859a1d37b8076e0030853e41e8c
axquijanog <96125418+axquijanog@users.noreply.github.com>
2021-12-20 08:21:38 -0500

    MaquetadoHeaderFooter
    Subiendo cambios

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -5,6 +5,7 @@ import { LayoutRoutingModule } from './layout-routing.module';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { LayoutfullComponent } from './layoutfull/layoutfull.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { LayoutfullComponent } from './layoutfull/layoutfull.component';
   ],
   imports: [
     CommonModule,
-    LayoutRoutingModule
+    LayoutRoutingModule,
+    SharedModule
   ]
 })
 export class LayoutModule { }

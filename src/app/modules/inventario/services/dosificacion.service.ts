a79a34e158e69258718cf7fe47694986807fe388
axquijanog <96125418+axquijanog@users.noreply.github.com>
2021-12-26 01:15:24 -0500

    CrearDosificacion
    Se agregan cambios del crear dosificación.

---
+++ src/app/modules/inventario/services/dosificacion.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Dosificacion } from 'src/app/shared/models/dosificacion';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DosificacionService {
+
+  dosificacionData: Dosificacion[] = [
+    {id: 1, nombre: 'Hydrogen', descripcion: 'Descripcion', codigo: 'H'},
+    {id: 2, nombre: 'Hdro', descripcion: 'ututut', codigo: 'H'},
+    {id: 3, nombre: 'Hogen', descripcion: 'kjksss', codigo: 'H'},
+  ];
+
+  constructor() { }
+
+  //getDosificaciones(): Observable<Dosificacion[]> {
+  getDosificaciones(): Dosificacion[] {
+    //return this.http.get<Dosificacion[]>(environment.urlServer+'dosificacion/listar');
+    return this.dosificacionData;
+  }
+
+  //agregarDosificacion(dosificacion: Dosificacion):Observable<Dosificacion> {
+  agregarDosificacion(dosificacion: Dosificacion):Dosificacion {
+    //return this.http.post<Dosificacion>(environment.urlServer+'dosificacion/agregar', dosificacion);
+    dosificacion.id = this.dosificacionData.length+1;
+    this.dosificacionData.push(dosificacion);
+    return dosificacion;
+  }
+}

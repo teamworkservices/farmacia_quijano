7806923f4343f53674f4d5d3b2b60b7a5f3535e1
axquijanog <96125418+axquijanog@users.noreply.github.com>
2021-12-27 09:38:02 -0500

    EditarDosificacion
    Se suben cambios de editar  dosificación y login

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -6,13 +6,15 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { LayoutfullComponent } from './layoutfull/layoutfull.component';
 import { SharedModule } from '../shared/shared.module';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
     FooterComponent,
-    LayoutfullComponent
+    LayoutfullComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule,
